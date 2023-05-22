22e588f7b95fa5e5945a83e036775d3347c447bf
Saito Shunsuke <50902764+syun100000@users.noreply.github.com>
2023-05-23 04:58:36 +0900

    Initial commit

---
+++ BusSchedule_getter.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+import pytz
+from datetime import datetime,timedelta
+
+class BUS_GET:
+    def __init__(self, type):
+        self.type = type
+        url = 'https://www.nit.ac.jp/campus/access/bus-schedule'
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        if type=="JR_station":
+            elems = soup.select("#main > div > div.split-contents__main.bg-white.order-2 > div.bus_main > div.jr > table:nth-child(1)")
+        elif type=="JR_campus":
+            elems = soup.select("#main > div > div.split-contents__main.bg-white.order-2 > div.bus_main > div.jr > table:nth-child(2)")
+        elif type=="TOBU_station":
+            elems = soup.select("#main > div > div.split-contents__main.bg-white.order-2 > div.bus_main > div.tobu > table:nth-child(1)")
+        elif type=="TOBU_campus":
+            elems = soup.select("#main > div > div.split-contents__main.bg-white.order-2 > div.bus_main > div.tobu > table:nth-child(2)")
+        self.bus_schedule = {}
+
+        # We start at index 1 to skip the first column with the title
+        for i, header in enumerate(elems[0].find_all('th')[1:], start=1):
+            self.bus_schedule[f'{header.text}'] = []
+            departure_times = elems[0].find_all('td')[i].find_all('li')
+            for time in departure_times:
+                self.bus_schedule[f'{header.text}'].append(time.text)
+
+        self.timezone = pytz.timezone('Asia/Tokyo')
+        self.bus_schedule_list = []
+        for hour, minutes in self.bus_schedule.items():
+            for minute in minutes:
+                self.bus_schedule_list.append(datetime.strptime(f'{hour}:{minute}', '%H:%M'))
+        self.bus_schedule_list.sort()
+
+    def print(self):
+        # Print the bus schedule
+        for time, schedule in self.bus_schedule.items():
+            print(f'{time}: {schedule}')
+    
+    def next_bus(self):
+        now = datetime.now(self.timezone).time()  # We only care about the time
+        for bus_time in self.bus_schedule_list:
+            if bus_time.time() > now:
+                return bus_time.strftime('%H:%M')
+        return '本日の運行は終了しました。'
+    
+
+
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+from BusSchedule_getter import BUS_GET
+from datetime import datetime
+
+app = Flask(__name__)
+nowtime=datetime.now()
+print(nowtime)
+@app.route('/')
+def home():
+    bus_schedule_JR_S = BUS_GET("JR_station")
+    bus_schedule_JR_C = BUS_GET("JR_campus")
+    bus_schedule_TOBU_S = BUS_GET("TOBU_station")
+    bus_schedule_TOBU_C = BUS_GET("TOBU_campus")
+
+    next_bus_JR_S = bus_schedule_JR_S.next_bus()
+    next_bus_JR_C = bus_schedule_JR_C.next_bus()
+    next_bus_TOBU_S = bus_schedule_TOBU_S.next_bus()
+    next_bus_TOBU_C = bus_schedule_TOBU_C.next_bus()
+    
+
+    return render_template('index.html', 
+                           next_bus_JR_S=next_bus_JR_S, 
+                           next_bus_JR_C=next_bus_JR_C,
+                           next_bus_TOBU_S=next_bus_TOBU_S, 
+                           next_bus_TOBU_C=next_bus_TOBU_C,)
+
+if __name__ == '__main__':
+    app.run(debug=True)
